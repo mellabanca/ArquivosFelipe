f48a83edd43e583148976f3c3952e05c201aea0f
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-11-09 15:17:24 -0300

    versão 2

--- BaseClass.js
+++ BaseClass.js
@@ -1,5 +1,5 @@
-class Box {
-  constructor(x, y, width, height) {
+class BaseClass {
+  constructor(x, y, width, height, angle) {
     var options = {
         'restitution':0.8,
         'friction':1.0,
@@ -8,20 +8,17 @@ class Box {
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
+    this.image = loadImage("sprites/base.png");
     
     World.add(world, this.body);
   }
   display(){
-    var pos =this.body.position;
     var angle = this.body.angle;
     push();
-    translate(pos.x, pos.y);
+    translate(this.body.position.x, this.body.position.y);
     rotate(angle);
-    rectMode(CENTER);
-    strokeWeight(4);
-    stroke("green");
-    fill(255);
-    rect(0, 0, this.width, this.height);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
     pop();
   }
 };
